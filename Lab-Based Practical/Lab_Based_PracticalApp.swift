6e63c9c265146e08053c90d57bed8486ecb58153
Denidu Gamage <denidugamage21@gmail.com>
2024-11-23 19:24:56 +0530

    Initial Commit

---
+++ Lab-Based Practical/Lab_Based_PracticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_Based_PracticalApp.swift
+//  Lab-Based Practical
+//
+//  Created by Denidu Gamage on 2024-11-23.
+//
+
+import SwiftUI
+
+@main
+struct Lab_Based_PracticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab-Based PracticalTests/Lab_Based_PracticalTests.swift
@@ -0,0 +1,16 @@
+//
+//  Lab_Based_PracticalTests.swift
+//  Lab-Based PracticalTests
+//
+//  Created by Denidu Gamage on 2024-11-23.
+//
+
+import Testing
+
+struct Lab_Based_PracticalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
